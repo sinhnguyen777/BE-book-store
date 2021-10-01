9c54979e7446e056afe0502748e39c68ff25e978
công nguyễn đức <duccongnguyen14102001@gmail.com>
2021-10-01 17:17:59 +0700

    .

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -3,18 +3,83 @@ const catalogsRouter = require('./catalogs.route');
 const feedbacksRouter = require('./feedback.route');
 const discountCodeRouter = require('./discountCode.route');
 const usersRouter = require('./user.route');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerOptions={
+    swaggerDefinition:{
+        infor:{
+            title:'Customer API',
+            description:"Cumtomer API information",
+            contact:{
+                name:"Amazing Deverloper"
+            },
+            servers:["http://localhost:3000"]
+        }
+    },
+    apis:["index.route.js"]
+}
+
+
+
 
 function route(app) {
     
+    const swaggerDocs = swaggerJsDoc(swaggerOptions);
+    app.use('/api-doc',swaggerUi.serve,swaggerUi.setup(swaggerDocs));
     app.use('/feedbacks', feedbacksRouter);
     app.use('/users', usersRouter);
     app.use('/discountCodes', discountCodeRouter);
     app.use('/products', productsRouter);
     app.use('/catalogs', catalogsRouter);
-
-
-
-    
 }
 
 module.exports = route;
+
+/**
+ * @swagger
+ * /catalogs:
+ *   get:
+ *     summary: Register as user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: must be unique
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 minLength: 8
+ *                 description: At least one number and one letter
+ *             example:
+ *               name: fake name
+ *               email: fake@example.com
+ *               password: password1
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   $ref: '#/components/schemas/User'
+ *                 tokens:
+ *                   $ref: '#/components/schemas/AuthTokens'
+ *   
+ *         
+ */
