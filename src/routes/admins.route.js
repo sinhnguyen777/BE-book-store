c85e0d6a688fb371ba25088442f14cee70a8d684
hothilan0101 <90611249+hothilanstm01@users.noreply.github.com>
2021-10-18 21:10:08 +0700

    err

--- src/routes/admins.route.js
+++ src/routes/admins.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const adminsController = require('../app/controllers/admins.controller');
 const middlewares = require('../app/middleware/authencation')
 
-router.delete('/del/:id',middlewares.checkAuthencation,middlewares.checkAdmin,adminsController.delete);
+// router.delete('/del/:id',middlewares.checkAuthencation,middlewares.checkAdmin,adminsController.delete);
 // router.put('/edit/:id', adminsController.update);
 router.post('/register',middlewares.checkAuthencation,middlewares.checkAdmin, adminsController.Register);
 router.post('/login', adminsController.Login);
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -10,26 +10,6 @@ const catalogsRouter = require('./catalogs.route');
 const feedbacksRouter = require('./feedback.route');
 const discountCodeRouter = require('./discountCode.route');
 const usersRouter = require('./users.route');
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
-
-const swaggerOptions={
-    swaggerDefinition:{
-        infor:{
-            title:'Customer API',
-            description:"Cumtomer API information",
-            contact:{
-                name:"Amazing Deverloper"
-            },
-            servers:["http://localhost:3000"]
-        }
-    },
-    apis:["index.route.js"]
-}
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-
-
 
 function route(app) {
     // app.use('/', function(req,res,next){
@@ -51,50 +31,4 @@ function route(app) {
 
 module.exports = route;
 
-/**
- * @swagger
- * /catalogs:
- *   get:
- *     summary: Register as user
- *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - name
- *               - email
- *               - password
- *             properties:
- *               name:
- *                 type: string
- *               email:
- *                 type: string
- *                 format: email
- *                 description: must be unique
- *               password:
- *                 type: string
- *                 format: password
- *                 minLength: 8
- *                 description: At least one number and one letter
- *             example:
- *               name: fake name
- *               email: fake@example.com
- *               password: password1
- *     responses:
- *       "201":
- *         description: Created
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 user:
- *                   $ref: '#/components/schemas/User'
- *                 tokens:
- *                   $ref: '#/components/schemas/AuthTokens'
- *   
- *         
- */
+
