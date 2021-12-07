d1dcfd5a3d4f6360b4c7658263da083ea699bd6c
hothilan0101 <90611249+hothilanstm01@users.noreply.github.com>
2021-12-07 18:53:34 +0700

    Update vip.model.js

--- src/app/models/vip.model.js
+++ src/app/models/vip.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const Vip = new Schema({
     name: { type: String, required: true },
-    price: { type: String, required: true },
+    price: { type: Number, required: true },
     time: { type: Number, required: true }
 }, {
     timestamps: true,
