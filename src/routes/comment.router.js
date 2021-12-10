19b799a5133b8f2cef0712d09a470f54fb449297
hothilan0101 <90611249+hothilanstm01@users.noreply.github.com>
2021-12-10 19:57:04 +0700

    Update comment.router.js

--- src/routes/comment.router.js
+++ src/routes/comment.router.js
@@ -5,8 +5,8 @@ const middlewares = require('../app/middleware/authencation')
 const commentController = require('../app/controllers/comment.controller');
 
 router.delete('/del/:id',middlewares.checkAuthencation,middlewares.checkRoleDelCata,commentController.delete);
-router.put('/edit', commentController.update);
-router.post('/create', commentController.create);
+router.put('/edit',middlewares.checkAuthencation, commentController.update);
+router.post('/create',middlewares.checkAuthencation, commentController.create);
 router.get('/:id', commentController.getByIdCata);
 router.get('/idProduct/:id', commentController.GetAllByIdProduct);
 
