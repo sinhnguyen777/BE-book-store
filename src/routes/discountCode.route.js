76fe98c3d02e73704b94405cde076d98fde1b0d1
hothilan0101 <90611249+hothilanstm01@users.noreply.github.com>
2021-12-13 20:42:32 +0700

    coupon

--- src/routes/discountCode.route.js
+++ src/routes/discountCode.route.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const middlewares = require('../app/middleware/authencation')
 
 const discountCodeController = require('../app/controllers/discountCode.controller');
 
-router.delete('/del/:id', discountCodeController.delete);
-router.put('/edit/:id', discountCodeController.update);
+router.delete('/del/:id',middlewares.checkAuthencation,discountCodeController.delete);
+router.put('/edit', discountCodeController.update);
 router.post('/create', discountCodeController.create);
-router.get('/', discountCodeController.index);
+router.get('/:id', discountCodeController.getByIdDiscount);
+router.get('/', discountCodeController.GetAll);
 
 
 module.exports = router;
