c40b3fb40cace4541ab95a6dde1d5c8787bea68b
công nguyễn đức <duccongnguyen14102001@gmail.com>
2021-12-20 17:41:31 +0700

    change User

--- src/app/controllers/orders.controller.js
+++ src/app/controllers/orders.controller.js
@@ -35,6 +35,10 @@ module.exports.NewOrder = async (req, res, next) => {
     try {
         const value = req.body;
         const idUser = value.idUser;
+        const code = value.code;
+        if (code) {
+            await DiscountCodeService.delete(code)
+        }
         const checkUser = await UserService.getById(idUser)
         if (!checkUser) {
             value.cancel = true;
@@ -221,9 +225,9 @@ module.exports.NewOrder = async (req, res, next) => {
                             quantity: item.quantity,
                             price: item.price,
                         }
-                        const order =  await OrderDetailService.newOrder(newValue)
+                        const order = await OrderDetailService.newOrder(newValue)
                         console.log(order);
-                        return res.status(200).json({ code: "200", message: "sucsses",data:order })
+                        return res.status(200).json({ code: "200", message: "sucsses", data: order })
 
                     }
                 });
@@ -249,6 +253,148 @@ module.exports.confirm = async (req, res, next) => {
                 status: true
             }
             await OrderService.update(id, value);
+            console.log(checkOrder);
+            const nodemailer = require('nodemailer')
+
+            var transporter = nodemailer.createTransport({ // config mail server
+                service: "gmail",
+                auth: {
+                    user: 'chapterone.bookstoreteam@gmail.com',
+                    pass: 'bookstore'
+                },
+                tls: { rejectUnauthorized: false }
+            });
+
+            var mainOptions = {
+                from: 'chapterone.bookstoreteam@gmail.com',
+                to: checkOrder.email,
+                subject: `Kính gửi Ông/Bà ${checkOrder.fullName}.`,
+                text: `Xác nhận`,
+                html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" />
+                <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+                  <head> </head>
+                  <head>
+                    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <meta name="x-apple-disable-message-reformatting" />
+                    <!--[if !mso]><!-->
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                    <!--<![endif]-->
+                    <style type="text/css">
+                      * {
+                        text-size-adjust: 100%;
+                        -ms-text-size-adjust: 100%;
+                        -moz-text-size-adjust: 100%;
+                        -webkit-text-size-adjust: 100%;
+                      }
+                
+                      html {
+                        height: 100%;
+                        width: 100%;
+                      }
+                
+                      body {
+                        height: 100% !important;
+                        margin: 0 !important;
+                        padding: 0 !important;
+                        width: 100% !important;
+                        mso-line-height-rule: exactly;
+                      }
+                
+                      div[style*="margin: 16px 0"] {
+                        margin: 0 !important;
+                      }
+                
+                      table,
+                      td {
+                        mso-table-lspace: 0pt;
+                        mso-table-rspace: 0pt;
+                      }
+                
+                      img {
+                        border: 0;
+                        height: auto;
+                        line-height: 100%;
+                        outline: none;
+                        text-decoration: none;
+                        -ms-interpolation-mode: bicubic;
+                      }
+                
+                      .ReadMsgBody,
+                      .ExternalClass {
+                        width: 100%;
+                      }
+                
+                      .ExternalClass,
+                      .ExternalClass p,
+                      .ExternalClass span,
+                      .ExternalClass td,
+                      .ExternalClass div {
+                        line-height: 100%;
+                      }
+                    </style>
+                    <!--[if gte mso 9]>
+                      <style type="text/css">
+                      li { text-indent: -1em; }
+                      table td { border-collapse: collapse; }
+                      </style>
+                      <![endif]-->
+                    <title>Welcome to HEML!</title>
+                    <!-- content -->
+                    <!--[if gte mso 9]><xml>
+                       <o:OfficeDocumentSettings>
+                        <o:AllowPNG/>
+                        <o:PixelsPerInch>96</o:PixelsPerInch>
+                       </o:OfficeDocumentSettings>
+                      </xml><![endif]-->
+                  </head>
+                  <body class="body" style="margin: 0; width: 100%;">
+                    <table class="bodyTable" role="presentation" width="100%" align="left" border="0" cellpadding="0" cellspacing="0" style="width: 100%; margin: 0;">
+                      <tr>
+                        <td class="body__content" align="center" width="100%" valign="top" style="color: #000000; font-family: Helvetica,Arial,sans-serif; font-size: 16px; line-height: 20px; text-align: center;">
+                          <div class="container" style="margin: 0 auto; max-width: 600px; width: 100%;"> <!--[if mso | IE]>
+                            <table class="container__table__ie" role="presentation" border="0" cellpadding="0" cellspacing="0" style="margin-right: auto; margin-left: auto;width: 600px" width="600" align="center">
+                              <tr>
+                                <td> <![endif]-->
+                                  <table class="container__table" role="presentation" border="0" align="center" cellpadding="0" cellspacing="0" width="100%">
+                                    <tr class="container__row">
+                                      <td class="container__cell" width="100%" align="left" valign="top">
+                                        <p class="logo text p" style="margin: 14px 0; color: #000000; font-family: Helvetica,Arial,sans-serif; font-size: 16px; line-height: 20px; text-align: center; width: 100%; justify-content: center; align-items: center; display: flex;">
+                                        <img src="https://chapterone.qodeinteractive.com/wp-content/uploads/2019/08/logo.png" border="0" alt="" class="img__block" style="display: block; max-width: 100%;" />
+                                          <h1 class="header h1" style="margin: 20px 0; line-height: 40px; font-family: Helvetica,Arial,sans-serif; text-align: center; color: #FF0000;">ChapterOne</h1>
+                                        </p>
+                                        <div class="confirm" style="align-items: center; border: 1px solid; display: flex; font-family: 'Montserrat',sans-serif; justify-content: center; position: relative;">
+                                          <div class="error-container" style="color: #343434; padding: 40px; text-align: center; width: 90%;">
+                                            <p class="thanks text p" style="display: block; margin: 14px 0; font-family: Helvetica,Arial,sans-serif; line-height: 20px; text-align: center; color: #0F1568; font-size: 19px;">Cám ơn bạn đã đặt hàng tại <strong>ChapterOne</strong></p>
+                                            <p class="hello text p" style="display: block; margin: 14px 0; color: #000000; font-family: Helvetica,Arial,sans-serif; line-height: 20px; font-size: 19px; text-align: left;">Xin chào</p>
+                                            <p class="content text p" style="display: block; margin: 14px 0; color: #000000; font-family: Helvetica,Arial,sans-serif; line-height: 20px; font-size: 15px; text-align: left;"> Đơn hàng của bạn đã được chúng tôi giao cho đơn vị vận chuyển. Cám ơn bạn vì đã mua hàng tại ChapterOne </p>
+                                            <p class="xacnhan text p" style="display: block; margin: 14px 0; color: #000000; font-family: Helvetica,Arial,sans-serif; font-size: 16px; line-height: 20px; text-align: center; padding-top: 20px;">
+                                            <a class="btn a" to="/" href=${`http://localhost:3000/side/order-detail/${checkOrder._id}`} " style="transition: all .5s; padding: 10px 20px; margin: 20px 0; border-radius: 5px; background-color: #000000; color: #FFFFFF; text-decoration: none;"><span class="a__text" style="color: #FFFFFF; text-decoration: none;">Xem đơn hàng</span></a>                              </p>
+                                          </div>
+                                        </div>
+                                      </td>
+                                    </tr>
+                                  </table> <!--[if mso | IE]> </td>
+                              </tr>
+                            </table> <![endif]--> </div>
+                        </td>
+                      </tr>
+                    </table>
+                    <div style="display:none; white-space:nowrap; font-size:15px; line-height:0;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </div>
+                  </body>
+                </html>
+                `
+            }
+
+            transporter.sendMail(mainOptions, function (err, info) {
+                if (err) {
+                    console.log(err);
+                    console.log(`can't send mail`);
+                } else {
+                    console.log({ code: "200", 'Message sent: ': info.response })
+                }
+            });
+
             res.status(200).json({ code: "200", message: "xác nhận gửi thành công" });
 
         }
